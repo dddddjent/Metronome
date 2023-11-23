1fa131a91bc58feb5dfd7b46e154a3e1e337e321
dddddjent <1306043330a@gmail.com>
2023-11-23 14:45:19 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/metronome/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.metronome
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.lang.Error
+import java.lang.NumberFormatException
+
+class MainActivity : AppCompatActivity() {
+    val Tag = "MainActivity"
+    lateinit var soundManager: SoundManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        soundManager = SoundManager(this)
+
+        val playButton = findViewById<Button>(R.id.play_button)
+        playButton.setOnClickListener {
+            soundManager.run()
+            Log.i(Tag, "Start playing")
+        }
+
+        val stopButton = findViewById<Button>(R.id.stop_button)
+        stopButton.setOnClickListener {
+            soundManager.stop()
+            Log.i(Tag, "Stop playing")
+        }
+
+        val setButton = findViewById<Button>(R.id.set_button)
+        setButton.setOnClickListener {
+            val text = findViewById<TextView>(R.id.bpm_text).text.toString()
+            val bpm = try {
+                text.toLong()
+            } catch (e: NumberFormatException) {
+                Log.e(Tag, "not an integer!")
+                return@setOnClickListener
+            }
+            soundManager.interval = bpmToIntervalMS(bpm)
+            if (soundManager.isRunning == true) {
+                soundManager.stop()
+                soundManager.run()
+            }
+            Log.i(Tag, "bpm: $bpm")
+        }
+    }
+
+    private fun bpmToIntervalMS(bpm: Long): Long {
+        return 60000 / bpm
+    }
+}
---
+++ app/src/main/java/com/example/metronome/SoundManager.kt
@@ -0,0 +1,66 @@
+package com.example.metronome
+
+import android.media.AudioAttributes
+import android.media.SoundPool
+import android.util.Log
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledFuture
+import java.util.concurrent.TimeUnit
+
+class SoundManager(val context: MainActivity) {
+    private lateinit var mTask: Runnable
+    private var soundPool: SoundPool
+    private val soundID = HashMap<String, Int>()
+    private val maxStreams = 5
+    var interval = 1000L
+    var isRunning = false
+    private val executionService = Executors.newScheduledThreadPool(3)
+    private var futureTask: ScheduledFuture<*>
+
+    init {
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_MEDIA)
+            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+            .build()
+        soundPool = SoundPool.Builder()
+            .setMaxStreams(maxStreams)
+            .setAudioAttributes(audioAttributes)
+            .build()
+
+        soundID["s0"] = soundPool.load(context, R.raw.s0, 1)
+        soundID["s1"] = soundPool.load(context, R.raw.s1, 1)
+
+        initTask()
+
+        futureTask =
+            executionService.scheduleAtFixedRate(mTask, 1000, interval, TimeUnit.MILLISECONDS)
+        futureTask.cancel(true)
+    }
+
+    fun initTask() {
+        mTask = Runnable {
+            soundID["s0"]?.let { soundPool.play(it, 1f, 1f, 0, 0, 1f) }
+        }
+    }
+
+    fun run() {
+        if (futureTask.isCancelled == true || futureTask.isDone == true) {
+            futureTask =
+                executionService.scheduleAtFixedRate(mTask, 0, interval, TimeUnit.MILLISECONDS)
+            isRunning = true
+            Log.d(context.Tag, "Task starts")
+        } else {
+            Log.d(context.Tag, "Task is already running")
+        }
+    }
+
+    fun stop() {
+        if (futureTask.isCancelled == false) {
+            futureTask.cancel(true)
+            isRunning = false
+            Log.d(context.Tag, "Task is stopped")
+        } else {
+            Log.d(context.Tag, "Task is already not running")
+        }
+    }
+}
